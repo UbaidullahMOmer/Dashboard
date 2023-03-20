56426d944cc1c9041e050489c02c138e74e0e185
Ubaidullah <researchfigma@gmail.com>
2023-03-20 18:59:14 +0500

    data grid completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
 
-function App() {
+const columns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'phone', headerName: 'Phone', width: 150 },
+  { field: 'address', headerName: 'Address', width: 150 },
+  { field: 'city', headerName: 'City', width: 150 },
+  { field: 'zipCode', headerName: 'ZipCode', width: 150 }
+]
+
+
+const DataGridd = () => {
+
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3333/rows")
+      .then((data) => data.json())
+      .then((data) => setTableData(data))
+
+  }, [])
+
+  console.log(tableData)
+
   return (
-    <div className="app">
-      <h1>Ubaidullah</h1>
+    <div style={{ height: 700, width: '100%' }}>
+      <DataGrid
+        rows={tableData}
+        columns={columns}
+        pageSize={12}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default DataGridd
